1ef89faa1da3288815325bab76a41182f9228767
Asgeir Nilsen <asgeir@fintlabs.no>
2019-03-05 11:00:56 +0100

    Added deployment for beta as well.

---
+++ deployment/beta.ps1
@@ -0,0 +1,12 @@
+$version = "build.3"
+$baseuri = "https://beta.felleskomponent.no"
+
+$component = "administrasjon"
+foreach ($stack in "personal", "organisasjon", "kodeverk") {
+    cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+}
+
+$component = "utdanning"
+foreach ($stack in "elev", "utdanningsprogram", "vurdering", "timeplan", "kodeverk") {
+    cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+}
