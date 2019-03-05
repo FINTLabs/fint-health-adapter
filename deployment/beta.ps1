2dcea6c0657058fc12481704e5f394238b6fa331
Asgeir Nilsen <asgeir@fintlabs.no>
2019-03-05 20:19:41 +0100

    Added fullmakt and reduced container sizes.

--- deployment/beta.ps1
+++ deployment/beta.ps1
@@ -2,7 +2,7 @@ $version = "build.3"
 $baseuri = "https://beta.felleskomponent.no"
 
 $component = "administrasjon"
-foreach ($stack in "personal", "organisasjon", "kodeverk") {
+foreach ($stack in "personal", "organisasjon", "kodeverk", "fullmakt") {
     cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
 }
 
