c0b9084d2b4232075078501fa4c2d97c85d6df80
Asgeir Nilsen <asgeir@fintlabs.no>
2019-03-06 14:31:18 +0100

    Added production.
    TODO: Remove temporary host clause.

---
+++ deployment/api.ps1
@@ -0,0 +1,14 @@
+$version = "build.7"
+$baseuri = "https://api.felleskomponent.no"
+
+cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME felles-kodeverk -p BASE_URI ${baseuri}/felles/kodeverk -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+
+$component = "administrasjon"
+foreach ($stack in "personal", "organisasjon", "kodeverk", "fullmakt") {
+    cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+}
+
+$component = "utdanning"
+foreach ($stack in "elev", "utdanningsprogram", "vurdering", "timeplan", "kodeverk") {
+    cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+}
