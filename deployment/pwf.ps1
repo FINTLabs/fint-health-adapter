26312b8fd18d999e695ad813829aa6c32361566f
Asgeir Nilsen <asgeir@fintlabs.no>
2019-09-09 12:17:41 +0200

    pwf: Added fullmakt

--- deployment/pwf.ps1
+++ deployment/pwf.ps1
@@ -4,7 +4,7 @@ $baseuri = "https://play-with-fint.felleskomponent.no"
 cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME felles-kodeverk -p BASE_URI ${baseuri}/felles/kodeverk -p VERSION $version | kubectl --namespace=pwf apply -f -
 
 $component = "administrasjon"
-foreach ($stack in "personal", "organisasjon", "kodeverk") {
+foreach ($stack in "personal", "organisasjon", "kodeverk", "fullmakt") {
     cat .\template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version | kubectl --namespace=pwf apply -f -
 }
 
