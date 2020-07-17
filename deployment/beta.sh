9c8f6650a6ba33e7e0906998d09794ccad867b42
Asgeir Nilsen <asgeir@fintlabs.no>
2020-07-17 14:32:56 +0200

    Migrate deployments to shell scripts.

---
+++ deployment/beta.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+version="build.12"
+baseuri="https://beta.felleskomponent.no"
+
+cat template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME felles-kodeverk -p BASE_URI ${baseuri}/felles/kodeverk -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+cat template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME utdanning-vigokodeverk -p BASE_URI ${baseuri}/utdanning/vigo/kodeverk -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+
+component="administrasjon"
+for stack in "personal" "organisasjon" "kodeverk" "fullmakt" "arkiv"
+do
+    cat template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+done
+
+component="utdanning"
+for stack in "elev" "utdanningsprogram" "vurdering" "timeplan" "kodeverk"
+do
+    cat template.yaml | docker run -i jimmycuadra/ktmpl - -p SERVICE_NAME ${component}-${stack} -p BASE_URI ${baseuri}/${component}/${stack} -p VERSION $version -p ENABLE_OAUTH true | kubectl apply -f -
+done
